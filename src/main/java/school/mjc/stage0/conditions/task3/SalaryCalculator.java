9671d8543a5e859cd350f0d7cdf3d4e77c088d92
Madara <ranel28021998@gmail.com>
2022-12-22 22:06:03 +0300

    It's done! Again..

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -4,14 +4,14 @@ public class SalaryCalculator {
     public void calculateSalary(int salary) {
         int sum = salary;
         if (salary <= 0 ){
-            sum = 0;
+            System.out.println("wrong input!");
+            return;
         }else if (salary <= 10000) {
             sum -= (salary * 0.15);
         } else if (salary <= 20000) {
             sum -= (salary * 0.18);
         } else {
-            System.out.println("wrong input!");
-            return;
+            sum -= (salary * 0.2);
         }
         System.out.println(sum);
     }
